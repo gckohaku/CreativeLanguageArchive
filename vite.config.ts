a930fd02774d14a608999339515cc6cec1228f80
gckohaku <cyanhairneko@gmail.com>
2023-11-15 23:26:09 +0900

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {resolve} from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +13,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+		outDir: 'docs',
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				404: resolve(__dirname, '404.html')
+			},
+			output: {
+				entryFileNames: `assets/[name]-[hash].js`,
+				chunkFileNames: `assets/[name]-[hash].js`,
+				assetFileNames: `assets/[name]-[hash].[ext]`,
+			}
+		},
+	},
+
 })
