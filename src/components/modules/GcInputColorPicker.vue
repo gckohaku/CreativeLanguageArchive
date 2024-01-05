1f2f4511d7eaeb6bf57c69618230638e4f77abed
gckohaku <cyanhairneko@gmail.com>
2024-01-06 00:18:55 +0900

    Squashed 'webclock/' content from commit 42b866b
    git-subtree-dir: webclock
    git-subtree-split: 42b866bde0bb75812e442c6ee9243fb9e16623db

---
+++ src/components/modules/GcInputColorPicker.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+export interface Props {
+	name?: string;
+	id?: string;
+	modelValue: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+	name: "",
+	id: "",
+})
+
+const emit = defineEmits<{
+	"update:modelValue": [value: string],
+}>();
+</script>
+
+<template>
+	<div class="color-picker-wrapper">
+		<input type="color" class="color-picker" :value="modelValue" @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+		<input type="text" class="color-text" :value="modelValue" @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+	</div>
+</template>
+
+<style scoped lang="scss">
+.color-picker-wrapper {
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+
+	.color-picker, .color-text {
+		padding: 2px 1px;
+	}
+
+	.color-picker {
+		border: 2px solid var(--colorPickerBorderColor);
+		border-radius: 10px;
+		cursor: pointer;
+
+		::-webkit-color-swatch {
+			height: 1.5rem;
+		}
+	}
+
+	.color-text {
+		width: 4rem;
+		border: 2px solid var(--colorTextBorderColor);
+		border-radius: 10px;
+		padding: .25rem .1rem;
+		outline: none;
+		text-align: center;
+	}
+}
+</style>
---
+++ src/components/modules/GcSelectInput.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+export interface Props {
+	name?: string,
+	id?: string,
+	modelValue: string,
+};
+
+const props = withDefaults(defineProps<Props>(), {
+	name: "",
+	id: "",
+});
+
+const emit = defineEmits<{
+	"update:modelValue": [value: string]
+}>();
+</script>
+
+<template>
+	<div class="select-wrapper">
+		<select name="" id="" :value="modelValue" @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+			<slot></slot>
+		</select>
+	</div>
+</template>
+
+<style scoped lang="scss">
+/* style here */
+</style>
