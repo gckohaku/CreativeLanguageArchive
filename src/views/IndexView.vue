926471f8ec77adc0410c9b2a57ffc4bcba0114e4
gckohaku <cyanhairneko@gmail.com>
2024-01-09 03:34:37 +0900

    rename file
    to ContentsList.vue from ContentList.vue

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -6,7 +6,7 @@ import pushContentData from "@/page-contents/index"
 import { onBeforeMount, onMounted } from 'vue';
 import { contentData } from '@/common/contentData';
 import ContentInfoCard from '@/components/ContentInfoCard.vue';
-import ContentList from '@/components/ContentList.vue';
+import ContentList from '@/components/ContentsList.vue';
 
 onBeforeMount(() => {
 	contentData.splice(0);
