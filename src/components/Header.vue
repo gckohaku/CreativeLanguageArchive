e4a327470316746a6e9b5edd6f1564e2fbca8946
gckohaku <cyanhairneko@gmail.com>
2024-01-13 19:14:04 +0900

    create header

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// script here
+</script>
+
+<template>
+	<div class="header-wrapper">
+		<div class="header-container"></div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.header-wrapper {
+	width: 100%;
+	height: 60px;
+	background-color: green;
+
+	.header-container {
+		margin-inline: auto;
+		width: var(--commonContentWidth);
+		height: 100%;
+		background-color: cyan;
+	}
+}
+</style>
